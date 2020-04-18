d35ff51f1184e6ae45292a4b517b978bc2b8d9f5
Mohamed Halim <mohamedhalim1998@gmail.com>
2020-04-18 18:36:59 +0200

    change theme to dark

--- app/src/main/java/com/mohamed/halim/essa/todolist/MainActivity.kt
+++ app/src/main/java/com/mohamed/halim/essa/todolist/MainActivity.kt
@@ -2,13 +2,16 @@ package com.mohamed.halim.essa.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         val tasksFragment =  TasksFragment()
+
         supportFragmentManager.beginTransaction().replace(R.id.fragment_container, tasksFragment).commit()
     }
 }
